d85c4dab7a080d2255a9d768013bdd42a8b76fa3
amttm <amt.tmg@gmail.com>
2016-04-22 16:08:47 +0545

    initial changes

---
+++ application/controllers/dataentry.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: amttm
+ * Date: 4/21/2016
+ * Time: 3:12 PM
+ */
+class dataentry extends CI_Controller
+{
+    public function index()
+    {
+    $this->load->view('DataEntry');
+    }
+}
---
+++ application/views/DataEntry.php
@@ -0,0 +1,16 @@
+<html>
+<title>
+
+</title>
+<head>
+
+</head>
+<body>
+<form>
+    <fieldset>
+        <legend>Full Name of Restaurant:</legend>
+        <textarea rows="3" cols="100"></textarea>
+    </fieldset>
+</form>
+</body>
+</html>
